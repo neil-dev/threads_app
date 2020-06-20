6116dbc70258c3dc4594c1e580ebe1a140b014cc
neil <suvranilduttabiswas@gmail.com>
2020-06-20 20:19:28 +0530

    login bloc added

---
+++ lib/login/create_account_button.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+import 'package:threads/user_repository.dart';
+import 'package:threads/register/register.dart';
+
+class CreateAccountButton extends StatelessWidget {
+  final UserRepository _userRepository;
+
+  CreateAccountButton({Key key, @required UserRepository userRepository}) : assert(userRepository != null), _userRepository = userRepository, super(key: key);
+
+
+  @override
+  Widget build(BuildContext context) {
+    return FlatButton(
+      child: Text('Create an Account'),
+      onPressed: () {
+        Navigator.of(context).push(MaterialPageRoute(builder: (context) {
+          return RegisterScreen(userRepository: _userRepository);
+        }),);
+      },
+    );
+  }
+}
